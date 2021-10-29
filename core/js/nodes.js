5a4a22bb3cbea8ffc8a91bbe17c98b22b132bf9f
Mike Mulhearn <mulhearn1022@gmail.com>
2021-10-29 11:57:07 -0400

    Prevent updates if the page is not active

--- core/js/nodes.js
+++ core/js/nodes.js
@@ -5,8 +5,8 @@ $(document).ready(function() {
             var currentDate = '[' + new Date().toUTCString() + '] ';
             console.log(currentDate + ' :: ' + 'started updating node stats');
             refresh_value('nodes_stats','nodes');
-            setTimeout(update_node_stats, 20000);
 		}
+		setTimeout(update_node_stats, 20000);
 	}
 
 	function update_table() {
@@ -14,15 +14,15 @@ $(document).ready(function() {
             var currentDate = '[' + new Date().toUTCString() + '] ';
             console.log(currentDate + ' :: ' + 'started refreshing table');
             table.ajax.reload();
-            setTimeout(update_table, 60000);
         }
+        setTimeout(update_table, 60000);
 	}
 
 	function update_ext_stats() {
 	    if (!document.hidden) {
             refresh_value('ext_stats','ext');
-            setTimeout(update_ext_stats, 20000);
         }
+        setTimeout(update_ext_stats, 20000);
 	}
 
 	// First load 
